c5c9c7f5275cd7c2f0fabe81f0ddc48f688011b0
jin9358 <leejh9358@naver.com>
2025-06-04 17:48:33 +0900

    feat: 20250604 async, await ...etc study

---
+++ javascript/05.js
@@ -0,0 +1,17 @@
+/**
+ * Javascript class
+ */
+
+class Student {
+    name;
+    age;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const newStudent1 = new Student('김준일', 32);
+console.log(newStudent)
+console.log(newStudent1.name);
---
+++ javascript/08.js
@@ -0,0 +1,52 @@
+const student = {
+    name: '김준일',
+    age: 32,
+    address : '부산 동래구',
+    phone : '010-9988-1916',
+};
+
+// 비구조 할당
+const { name, address } = student;
+console.log(name, address);
+
+// REST 문법
+const {age, phone, ...a} = student;
+console.log(age, phone, a);
+
+const numbers = [1,2,3,4];
+const [n1, n2, ...ns] = numbers;
+console.log(n1,n2, ns);
+
+// spread
+const newStudent = {
+    ...student,
+    gender: '남',
+}
+
+console.log(newStudent);
+
+// 배열 spread
+const newNumbers = [...numbers, 5, 6]; // 얕은복사 - 값만
+console.log(newNumbers);
+
+const newNumbers2 = numbers; // 깊은복사 - 주소값까지
+
+let names = [];
+
+function addName(name) {
+    names = [...names, name];
+}
+
+let obj = {
+    data1: "data1",
+    data2: "data2",
+}
+
+function addProps(props) {
+    obj = {
+        ...obj,
+        ...props
+    }
+}
+addProps({data3 : "data3", data4: "data4"});
+console.log(obj);
